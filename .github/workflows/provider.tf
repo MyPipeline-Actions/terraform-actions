b481c3845ad91d2c1a197e67a8cbca74e409625b
Satya <vvsatyanarayana.swarnapudi@gmail.com>
2026-03-13 17:53:56 +0530

    provider.tf
    changes has been made

--- .github/workflows/provider.tf
+++ .github/workflows/provider.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.84.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
+  # Configuration options
   region = "us-east-1"
 }
